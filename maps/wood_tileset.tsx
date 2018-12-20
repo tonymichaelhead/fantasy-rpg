7be7880575d9e5a8486e0b83026e8d00f3e782be
tonymichaelhead <tonymichaelhead@gmail.com>
2018-12-19 19:24:33 -0800

    Add castle and ground terrain

--- maps/wood_tileset.tsx
+++ maps/wood_tileset.tsx
@@ -1,4 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.1" name="wood_tileset" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="../img/wood_tileset.png" width="512" height="512"/>
+ <terraintypes>
+  <terrain name="Bare Ground" tile="158"/>
+ </terraintypes>
+ <tile id="141" terrain=",,,0"/>
+ <tile id="142" terrain=",,0,0"/>
+ <tile id="143" terrain=",,0,"/>
+ <tile id="157" terrain=",0,,0"/>
+ <tile id="158" terrain="0,0,0,0"/>
+ <tile id="159" terrain="0,,0,"/>
+ <tile id="173" terrain=",0,,"/>
+ <tile id="174" terrain="0,0,,"/>
+ <tile id="175" terrain="0,,,"/>
 </tileset>
