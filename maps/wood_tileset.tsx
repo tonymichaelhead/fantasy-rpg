d081389716b41002b70ccea30f63d4ac6f298ef5
tonymichaelhead <tonymichaelhead@gmail.com>
2018-12-19 21:27:38 -0800

    Add water terrain

--- maps/wood_tileset.tsx
+++ maps/wood_tileset.tsx
@@ -3,7 +3,21 @@
  <image source="../img/wood_tileset.png" width="512" height="512"/>
  <terraintypes>
   <terrain name="Bare Ground" tile="158"/>
+  <terrain name="Water" tile="68"/>
  </terraintypes>
+ <tile id="51" terrain=",,,1"/>
+ <tile id="52" terrain=",,1,1"/>
+ <tile id="53" terrain=",,1,"/>
+ <tile id="67" terrain=",1,,1"/>
+ <tile id="68" terrain="1,1,1,1"/>
+ <tile id="69" terrain="1,,1,"/>
+ <tile id="83" terrain=",1,,"/>
+ <tile id="84" terrain="1,1,,"/>
+ <tile id="85" terrain="1,,,"/>
+ <tile id="86" terrain="1,1,1,"/>
+ <tile id="87" terrain="1,1,,1"/>
+ <tile id="102" terrain="1,,1,1"/>
+ <tile id="103" terrain=",1,1,1"/>
  <tile id="141" terrain=",,,0"/>
  <tile id="142" terrain=",,0,0"/>
  <tile id="143" terrain=",,0,"/>
